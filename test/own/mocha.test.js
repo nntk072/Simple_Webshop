b2fe38da54ba30955dbce1eb8696237b8c173e75
Ayman Khan <ayman.khan@tuni.fi>
2023-11-11 18:54:14 +0200

    10.3 MVC - Mocha test cases for GitLab issues

---
+++ test/own/mocha.test.js
@@ -0,0 +1,68 @@
+const chai = require('chai');
+
+describe('Issue 1', () => {
+  it('Test Case 1 Description', () => {
+    throw new Error('Not implemented');
+  });
+
+  it('Test Case N Description', () => {
+    throw new Error('Not implemented');
+  });
+});
+
+describe('Issue 2', () => {
+  it('Test Case 1 Description', () => {
+    throw new Error('Not implemented');
+  });
+});
+
+describe('Issue 3', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+
+describe('Issue 4', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+describe('Issue 5', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+describe('Issue 6', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+describe('Issue 7', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+describe('Issue 8', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+describe('Issue 9', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+describe('Issue 10', () => {
+    it('Test Case 1 Description', () => {
+      throw new Error('Not implemented');
+    });
+});
+
+
